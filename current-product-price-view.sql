57dfa7bf7c36edaa83a34a082a8592f88b28479d
Angel Lyn <angelvlynx@gmail.com>
2026-04-20 23:42:54 +0000

    Update SQL view to include unit column and ORDER BY prodCode

--- current-product-price-view.sql
+++ current-product-price-view.sql
@@ -2,14 +2,15 @@
 CREATE OR REPLACE VIEW current_product_price AS
 SELECT p.prodCode,
        p.description,
+       p.unit,
        ph.unitPrice,
        ph.effDate
 FROM product p
-JOIN priceHist ph
-  ON p.prodCode = ph.prodCode
+JOIN priceHist ph ON ph.prodCode = p.prodCode
 WHERE ph.effDate = (
   SELECT MAX(effDate)
   FROM priceHist
   WHERE prodCode = p.prodCode
 )
-AND p.record_status = 'ACTIVE';
+AND p.record_status = 'ACTIVE'
+ORDER BY p.prodCode;
